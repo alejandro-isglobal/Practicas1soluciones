6cbaeebe2916aa471305abeb1e7d9bf0a0a51680
ALEJANDRO <acaceres@ISGLOBAL.LAN>
2024-02-02 22:25:36 +0100

    ...

--- Problema1.R
+++ Problema1.R
@@ -1,3 +1,11 @@
 #Codigo para problema 1
-
-plot(1:5)
+mis_dades=mtcars
+head(mis_dades)
+mean(mis_dades$qsec)
+median(mis_dades$drat)
+quantile(mis_dades$drat, 0.25)
+quantile(mis_dades$mpg, 0.18)
+quantile(mis_dades$mpg, 1-0.82)
+IQR(mis_dades$cyl)
+sd(mis_dades$cyl)
+var(mis_dades$qsec)
